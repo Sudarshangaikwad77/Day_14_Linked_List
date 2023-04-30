0a786e767645131b07008086704a031c28754d4d
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-04-30 15:22:24 +0530

    code added to UC_6 branch

--- src/main/java/com/bl/linkedList/LinkedList.java
+++ src/main/java/com/bl/linkedList/LinkedList.java
@@ -1,5 +1,4 @@
 package com.bl.linkedList;
-
 public class LinkedList {
     Node head;
 
@@ -13,18 +12,33 @@ public class LinkedList {
         }
     }
 
+    public void popLast() {
+        if (head == null) {
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         System.out.println("Welcome to Linked List Practice Problem");
 
         list.head = new Node(56);
-        Node second = new Node(70);
+        Node second = new Node(30);
+        Node third = new Node(70);
 
         list.head.next = second;
+        second.next = third;
 
-        Node newNode = new Node(30);
-        newNode.next = second;
-        list.head.next = newNode;
+        list.popLast();
 
         Node n = list.head;
         while (n != null) {
