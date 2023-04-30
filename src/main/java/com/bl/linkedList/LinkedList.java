176d08ef09f9ec384b52cdb770e36317f5754cf5
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-04-30 15:17:23 +0530

    code added to uc-5 branch

--- src/main/java/com/bl/linkedList/LinkedList.java
+++ src/main/java/com/bl/linkedList/LinkedList.java
@@ -13,14 +13,26 @@ public class LinkedList {
         }
     }
 
+    public void pop() {
+        if (head == null) {
+            return;
+        }
+        head = head.next;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         System.out.println("Welcome to Linked List Practice Problem");
+
         list.head = new Node(56);
         Node second = new Node(30);
         Node third = new Node(70);
+
         list.head.next = second;
         second.next = third;
+
+        list.pop();
+
         Node n = list.head;
         while (n != null) {
             System.out.print(n.data + " ");
