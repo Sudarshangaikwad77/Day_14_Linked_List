0a6288ca888f79c94e549359ab4daa3fbf62913c
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-04-30 14:53:11 +0530

    code added to UC-3

--- src/main/java/com/bl/linkedList/LinkedList.java
+++ src/main/java/com/bl/linkedList/LinkedList.java
@@ -1,8 +1,7 @@
 package com.bl.linkedList;
 
-public class LinkedList
-{
-    Node head; // head of the list
+public class LinkedList {
+    Node head;
 
     static class Node {
         int data;
@@ -17,15 +16,11 @@ public class LinkedList
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         System.out.println("Welcome to Linked List Practice Problem");
-
         list.head = new Node(56);
         Node second = new Node(30);
         Node third = new Node(70);
-
         list.head.next = second;
         second.next = third;
-
-        // print the linked list
         Node n = list.head;
         while (n != null) {
             System.out.print(n.data + " ");
@@ -33,4 +28,3 @@ public class LinkedList
         }
     }
 }
-
